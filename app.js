f712810337163207e0fe159795a00ba3f8733d95
SaraToth <saratoth101@gmail.com>
2025-07-08 18:16:09 +0900

    Feat: Create app server

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const path = require("node:path");
+const assetsPath = path.join(__dirname, "public");
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(express.static(assetsPath));
+
+ app.get("/", (req, res) => {
+    res.send("Hello");
+ });
+
+ app.use((err, req, res, next) => {
+    console.error(err);
+    res.status(500).send(err);
+ });
+
+ const PORT = process.env.PORT;
+ app.listen(PORT || 3000, () => {
+    console.log("App is running");
+ });
