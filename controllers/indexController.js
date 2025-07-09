0052b5cc63030c87f3e284b5b7bfb887601afc29
SaraToth <saratoth101@gmail.com>
2025-07-09 23:08:31 +0900

    Feat: Dashboard renders folders and files

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,3 +1,4 @@
+const prisma = require("../prisma/client");
 
 const getLandingPage = (req, res) => {
     if (req.isAuthenticated()) {
@@ -6,8 +7,28 @@ const getLandingPage = (req, res) => {
     return res.render("landingPage");
 };
 
-const getDashboard = (req, res) => {
-    return res.render("dashboard", {user: req.user});
+const getDashboard = async (req, res) => {
+    const userId = req.user?.id;
+
+    // Need to get all the folders
+    const folders = await prisma.folder.findMany({
+        where: {
+            userId: userId
+        },
+    });
+
+    // need to get all the files
+    const files = await prisma.file.findMany({
+        where: {
+            userId: userId,
+            folder: {
+                folderName: "My Files"
+            }
+        }
+    });
+
+    return res.render("dashboard", {user: req.user, folders, files});
 };
+
 
 module.exports = { getLandingPage, getDashboard };
