549ab0d4feb4421b1c130ef8298a9579f2e9a7f7
SaraToth <saratoth101@gmail.com>
2025-07-09 21:23:54 +0900

    Feat: Finish app session

--- app.js
+++ app.js
@@ -21,7 +21,10 @@ app.use(postToPatchOverride);
 app.use(
    expressSession({
       cookie: {
-         maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days in milliseconds
+         maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days in milliseconds
+         secure: process.env.NODE_ENV === "production",
+         httpOnly: true,
+         sameSite: "lax",
       },
       secret: process.env.SECRET,
       resave: false,
