1944888f55fe29651621d7076fcb2381c8a81286
SaraToth <saratoth101@gmail.com>
2025-07-09 19:47:38 +0900

    Feat: Complete index controller

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,10 +1,15 @@
 
 const getLandingPage = (req, res) => {
+    if (req.isAuthenticated()) {
+        return res.redirect("/dashboard");
+    }
     res.send("This is the landing page for unauthorized users");
+    //return res.render("landingPage");
 };
 
 const getDashboard = (req, res) => {
     res.send("This is the dashboard for authorized users");
+    // return res.render("dashboard");
 };
 
 module.exports = { getLandingPage, getDashboard };
