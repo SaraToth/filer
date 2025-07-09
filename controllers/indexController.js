672ebda7dedb1e8d4509d9d3ac00083ef39930dd
SaraToth <saratoth101@gmail.com>
2025-07-09 21:33:17 +0900

    Feat: Create index page views

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -3,13 +3,11 @@ const getLandingPage = (req, res) => {
     if (req.isAuthenticated()) {
         return res.redirect("/dashboard");
     }
-    res.send("This is the landing page for unauthorized users");
-    //return res.render("landingPage");
+    return res.render("landingPage");
 };
 
 const getDashboard = (req, res) => {
-    res.send("This is the dashboard for authorized users");
-    // return res.render("dashboard");
+    return res.render("dashboard");
 };
 
 module.exports = { getLandingPage, getDashboard };
