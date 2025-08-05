3cd5b40f3e7937508872a7afbc0139bcaa6cc9ef
SaraToth <saratoth101@gmail.com>
2025-08-05 17:36:49 +0900

    Refactor: deleteFile script to the files.js module
    deleteFile function was quickly implemented inside the files partial view, but then finalized and transfered to its own module for better readability

--- public/js/files.js
+++ public/js/files.js
@@ -1,21 +1,51 @@
-    const kebabTriggers = document.querySelectorAll(".kebab");
-    const kebabContent = document.querySelector(".kebab-content");
+const kebabTriggers = document.querySelectorAll(".kebab");
+const kebabContent = document.querySelector(".kebab-content");
 
-    kebabTriggers.forEach((kebabTrigger) => {
-        kebabTrigger.addEventListener("click", (e) => {
-            e.preventDefault();
-            const kebabContent = kebabTrigger.nextElementSibling; // Assumes the kebab-content immediately after
-            if (!kebabContent) return;
+kebabTriggers.forEach((kebabTrigger) => {
+    kebabTrigger.addEventListener("click", (e) => {
+        e.preventDefault();
+        const kebabContent = kebabTrigger.nextElementSibling; // Assumes the kebab-content immediately after
+        if (!kebabContent) return;
 
-            kebabContent.classList.toggle("show");
-        });
-    })
-
-
-    window.addEventListener("click", (e) => {
-        if (!e.target.closest(".kebab-dropdown")) {
-            document.querySelectorAll(".kebab-content.show").forEach(menu => {
-                menu.classList.remove("show");
-            });
-        }
+        kebabContent.classList.toggle("show");
     });
+})
+
+window.addEventListener("click", (e) => {
+    if (!e.target.closest(".kebab-dropdown")) {
+        document.querySelectorAll(".kebab-content.show").forEach(menu => {
+            menu.classList.remove("show");
+        });
+    }
+});
+
+// Append delete file function to each delete file button after DOM loads
+document.addEventListener("DOMContentLoaded", () => {
+    const deleteFileBtns = document.querySelectorAll(".delete-file-btn");
+    deleteFileBtns.forEach((btn) => {
+        btn.addEventListener("click", () => {
+            const fileId = btn.dataset.fileid;
+            deleteFile(fileId);
+        });
+    });
+});
+
+// Sends server request to delete file and handles frontend deletion UI
+const deleteFile = (fileId) => {
+    fetch(`/files/${fileId}/delete`, {
+        method: "DELETE",
+        headers: {
+            "Content-Type": "application/json",
+        },
+    })
+    .then((res) => {
+        if (res.ok) {
+            location.reload(); 
+        } else {
+            alert(`${fileId}`);
+        }
+    })
+    .catch((err) => {
+        console.error("Error:", err);
+    });
+}
