6e7a1975140705d18a9a2539d1e0ac46c66459ae
SaraToth <saratoth101@gmail.com>
2025-08-10 00:54:05 +0900

    Feat: Removed logic that was refactored

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -7,68 +7,9 @@ const getLandingPage = (req, res) => {
     return res.render("landingPage");
 };
 
-const getFolders = async (req, res, next) => {
-    const userId = req.user?.id;
-
-    // Current folder defaults to My Files
-    let currentFolder = await prisma.folder.findFirst({
-        where: {
-            userId: userId,
-            folderName: "My Files",
-        },
-    });
-
-    // Update current folder to user selected folder
-    if (req.params.folderId) {
-        const folderId = parseInt(req.params.folderId);
-        currentFolder = await prisma.folder.findUnique({
-            where: {
-                id: folderId,
-            },
-        });
-    };
-
-    const folders = await prisma.folder.findMany({
-        where: {
-            userId: userId
-        },
-    });
-    req.folders = folders;
-    req.currentFolder = currentFolder;
-    next();
-};
-
-const getFiles = async (req, res, next) => {
-    const userId = req.user?.id;
-
-    // Default folder is My Files
-    let files = await prisma.file.findMany({
-        where: {
-            userId: userId,
-            folder: {
-                folderName: "My Files"
-            }
-        }
-    });
-
-    // If user clicks a different folder:
-    if (req.params.folderId) {
-        const folderId = parseInt(req.params.folderId);
-
-        files = await prisma.file.findMany({
-            where: {
-                userId: userId,
-                folderId: folderId
-            },
-        });
-    }
-    req.files = files;
-    next();
-};
-
 const getDashboard = async (req, res) => {
 
-    return res.render("dashboard", {user: req.user, currentFolder: req.currentFolder, folders: req.folders, files: req.files});
+    return res.render("dashboard", {user: req.user, currentFolder: req.currentFolder, folders: req.folders, files: req.files, errors: null});
 };
 
-module.exports = { getLandingPage, getDashboard, getFolders, getFiles };
+module.exports = { getLandingPage, getDashboard };
